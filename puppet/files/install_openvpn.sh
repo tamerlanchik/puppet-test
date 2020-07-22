2e70209b466075eb568e311b1f79b34555692557
Andrey Kochnov <andreykochnov@yandex.ru>
2020-07-23 00:24:56 +0300

    service

--- puppet/files/install_openvpn.sh
+++ puppet/files/install_openvpn.sh
@@ -1,18 +1,30 @@
 cd /etc/openvpn/keys
-wget https://github.com/OpenVPN/easy-rsa/archive/master.zip
-unzip master.zip
+FILE = /etc/openvpn/keys/master.zip
+if [ -f "$FILE" ]; then
+    echo "master.zip is already downloaded"
+else
+    wget https://github.com/OpenVPN/easy-rsa/archive/master.zip
+fi
+
+DIR = /etc/openvpn/keys/easy-rsa-master/easyrsa3
+if [ -d "DIR" ]; then
+    echo "master.zip is already unzipped"
+else
+    unzip master.zip
+fi
+
 cd /etc/openvpn/keys/easy-rsa-master/easyrsa3
-mv vars.example vars
-./easyrsa init-pki
-./easyrsa build-ca
-./easyrsa gen-req server nopass
-./easyrsa sign-req server server < echo "yes"
-./easyrsa gen-dh
+# mv vars.example vars
+# ./easyrsa init-pki
+# ./easyrsa build-ca
+# ./easyrsa gen-req server nopass
+# ./easyrsa sign-req server server < echo "yes"
+# ./easyrsa gen-dh
 
-cp pki/ca.crt /etc/openvpn/ca.crt
-cp pki/dh.pem /etc/openvpn/dh.pem
-cp pki/issued/server.crt /etc/openvpn/server.crt
-cp pki/private/server.key /etc/openvpn/server.key
+# cp pki/ca.crt /etc/openvpn/ca.crt && \
+# cp pki/dh.pem /etc/openvpn/dh.pem && \
+# cp pki/issued/server.crt /etc/openvpn/server.crt && \
+# cp pki/private/server.key /etc/openvpn/server.key
 
-./easyrsa gen-req client nopass
-./easyrsa sign-req client client < echo "yes"
+# ./easyrsa gen-req client nopass
+# ./easyrsa sign-req client client < echo "yes"
--- puppet/puppet.pp
+++ puppet/puppet.pp
@@ -115,10 +115,43 @@ class openvpn {
       Package['openvpn'],
     ]
   }
-  exec { 'install openvpn':
-    require => File['install_openvpn.sh'],
-    command => "${home}/install_openvpn.sh"
-    subscribe => File['install_openvpn.sh']
+  # exec { 'install openvpn':
+  #   require => File['install_openvpn.sh'],
+  #   command => "${home}/install_openvpn.sh"
+  #   subscribe => File['install_openvpn.sh']
+  # }
+  file { '/etc/openvpn/server.conf':
+    path => "/etc/openvpn/server.conf",
+    ensure => file,
+    source => 'puppet:///files/openvpn/server.conf',
+    mode => 0755,
+    owner => $user,
+  }
+  file { '/etc/openvpn/ccd':
+    ensure => directory,
+    mode => 0755,
+    owner => $user,
+  }
+  file { '/var/log/openvpn':
+    ensure => directory,
+    mode => 0755,
+    owner => $user,
+  }
+  file { '/etc/openvpn/ccd/client':
+    ensure => file,
+    content => "iroute 192.168.20.0 255.255.255.0",
+    mode => 0755,
+    owner => $user,
+  }
+
+  service {'openvpn@server':
+    require => Package['openvpn'],
+    ensure => running,
+    enable => true,
+    subscribe => [
+      File['/etc/openvpn/ccd/client'],
+      File['/etc/openvpn/server.conf'],
+    ]
   }
 
 }

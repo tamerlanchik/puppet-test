b1c2a1dcb05a0bce84073149a8e74f0fde4977c8
Andrey K <andreykochnov@yandex.ru>
2020-08-08 15:54:47 +0300

    Update install_docker-compose.sh

--- init/install_docker-compose.sh
+++ init/install_docker-compose.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # install docker-compose
 
-curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 if [[ ! "$PATH" =~ /\/usr\/local\/bin\// ]]; then
   if [[ ! -f "/etc/profile.d/path.sh" ]]; then
-      echo "#!/bin/bash" >> /etc/profile.d/path.sh;
+      sudo echo "#!/bin/bash" >> /etc/profile.d/path.sh;
   fi
-  echo "pathmunge /usr/local/bin/ after" >> /etc/profile.d/path.sh
+  sudo echo "pathmunge /usr/local/bin/ after" >> /etc/profile.d/path.sh
 fi
 
 docker-compose --version

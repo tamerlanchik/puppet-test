d4b26e2ae234ca69b97de564c7ba22f7c2719c7f
Andrey K <andreykochnov@yandex.ru>
2020-08-08 15:36:36 +0300

    Update install-puppetserver_docker.sh

--- init/install-puppetserver_docker.sh
+++ init/install-puppetserver_docker.sh
@@ -10,16 +10,37 @@ fi
 
 if [[ ! -d "~/pupperware" ]]; then
   yum install -y unzip
-  unzip ~/pupperware.zip;
+  unzip ~/pupperware.zip -d /usr/src/;
   rm ~/pupperware.zip;
 fi
-
-cd ~/pupperware
 
 if [[ ! -f "/usr/local/bin/docker-compose" ]]; then
   ./install-docker-compose.sh;
 fi
 
+cat > /etc/systemd/system/docker-compose-puppetmaster.service <<- EOF
+# /etc/systemd/system/docker-compose-puppetmaste.service
+[Unit]
+Description=Docker Compose Puppetmaster
+Requires=docker.service
+After=docker.service
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+WorkingDirectory=/srv/docker
+ExecStart=/usr/local/bin/docker-compose up -d
+ExecStop=/usr/local/bin/docker-compose down
+TimeoutStartSec=0
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cd /usr/src/pupperware
+# First run requires DNS_ALT_NAMES for setup
 DNS_ALT_NAMES=$server_names /usr/local/bin/docker-compose up -d
+
+systemctl enable docker-compose-puppetmaster
 
 echo "Puppetmaster installed and switched on!"
